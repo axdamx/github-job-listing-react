54ce10cb32d0d70d09883f3e15a627e85f676b8d
axdamx <axdamx@gmail.com>
2020-10-14 18:11:59 +0800

    using params for fetching data

--- github-joblisting/src/useFetchJobs.js
+++ github-joblisting/src/useFetchJobs.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 const ACTIONS = {
     MAKE_REQUEST: 'make-request',
     GET_DATA: 'get-data',
-    ERROR: 'error'
+    ERROR: 'error',
+    UPDATE_HAS_NEXT_PAGE: 'update-has-next-page'
 }
 
 const BASE_URL = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json'
@@ -17,6 +18,8 @@ function reducer (state, action) {
         return {...state, loading: false, jobs: action.payload.jobs}
         case ACTIONS.ERROR:
         return {...state, loading: false, error: action.payload.error, jobs: []}
+        case ACTIONS.UPDATE_HAS_NEXT_PAGE:
+            return {...state, hasNextPage: action.payload.hasNextPage}
         default:
             return state
     }
@@ -38,8 +41,21 @@ export default function useFetchJobs(params, page) {
             if (axios.isCancel(e)) return 
             dispatch({type: ACTIONS.ERROR, payload: {error: e}})
         })
+
+        const cancelToken1 = axios.CancelToken.source()
+
+        axios.get(BASE_URL, {
+            cancelToken: cancelToken1.token,
+            params: {markdown: true, page: page + 1, ...params}
+        }).then(res=> {
+            dispatch({type: ACTIONS.UPDATE_HAS_NEXT_PAGE, payload: {hasNextPage: res.data.length !== 0}})
+        }).catch(e => {
+            if (axios.isCancel(e)) return 
+            dispatch({type: ACTIONS.ERROR, payload: {error: e}})
+        })
         return () => {
             cancelToken.cancel()
+            cancelToken1.cancel()
         }
     }, [params, page])
 
