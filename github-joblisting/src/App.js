bd49fc3b84e4dc447525b38300c4ae1afa25c100
axdamx <axdamx@gmail.com>
2020-10-14 16:21:29 +0800

    create custom hooks for fetching data, installing bootstrap

--- github-joblisting/src/App.js
+++ github-joblisting/src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React , {useState} from 'react';
+import useFetchJobs from './useFetchJobs';
+import { Container } from 'react-bootstrap'
+import Job from './Job';
 
 function App() {
-  return <h1> Hello World </h1>
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
+  const {jobs, loading, error } = useFetchJobs();
+  return (
+    <Container>
+      {loading && <h1> Loading </h1>}
+      {error && <h1> error </h1>}
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })}
+    </Container>
+  )
 }
 
 export default App;
---
+++ github-joblisting/src/Job.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Card} from 'react-bootstrap';
+function Job({job}) {
+    return (
+        <div>
+            {job.title}
+        </div>
+    )
+}
+
+export default Job
